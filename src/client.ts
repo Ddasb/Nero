0cbe68ffa7287430cc60eb4beb28fd32fb48eb4b
Ddasb <damien.bregieiro@gmail.com>
2023-12-11 14:15:30 +0100

    fix uppercase

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,17 @@
 import { Client, Collection, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 import { RegisterCommands } from "./registers/RegisterCommands";
 import { RegisterEventHandler } from "./registers/RegisterEventHandler";
+
 dotenv.config();
+
+export const database = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite"
+});
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
--- src/models/Channels.model.ts
+++ src/models/Channels.model.ts
@@ -1,1 +1,12 @@
-import { Sequelize } from "sequelize";
+import { DataTypes } from "sequelize";
+import { Database } from "../Database";
+
+const Channels = Database.define("channels", {
+  guild: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  alert: {
+    type: DataTypes.STRING
+  }
+});
