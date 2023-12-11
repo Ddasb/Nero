796357edd7c08543adfeea968307aa8be5fee913
Ddasb <damien.bregieiro@gmail.com>
2023-12-11 11:19:41 +0100

    first command

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,9 +1,11 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+const cooldown = 5;
+
 const data = new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!");
 
 const execute = async (interaction) => {
   await interaction.reply("Pong!");
 };
 
-module.exports = { data, execute };
+module.exports = { cooldown, data, execute };
